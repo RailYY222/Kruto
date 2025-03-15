bf4eddbbcc6409f8b0ae73571c6d20e6954a6e1a
stud <stud@it224-11.almetpt.local>
2025-03-15 13:42:32 +0300

    Autocommit

---
+++ git_push.sh
@@ -0,0 +1,8 @@
+#1/bin/bash
+echo "Добавляю файлы в индексацию"
+git add .
+echo "Фиксируем изменения"
+git commit -m "Autocommit"
+echo "Отправляем на удаленный репозиторий..."
+git push origin "$1"
+echo "Готово!"
